e5e119a7dd30753cebe477103f3eff9684facb0e
Pär Nygren <par.nygren@sogeti.se>
2018-07-26 15:40:40 +0200

    Refactoring by using destructuring arguments and object destructuring.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,6 +3,7 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, onDelete, onIncrement } = this.props;
     return (
       <div>
         <button
